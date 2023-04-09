3fec8eeb365be8b0c162749b1a65235667594d95
Dhalici <deryay_70@hotmail.com>
2023-04-08 21:29:55 -0400

    Start working Econsent page(not done yet)

--- src/test/java/pages/EConsentPage.java
+++ src/test/java/pages/EConsentPage.java
@@ -1,4 +1,62 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.Driver;
+
 public class EConsentPage {
+
+    public EConsentPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+    @FindBy (id = "eConsentdeclarerFirstName")
+    private WebElement firstName;
+
+    @FindBy ( id="eConsentdeclarerLastName")
+    private WebElement lastName;
+
+    @FindBy ( id= "eConsentdeclarerEmail")
+    private WebElement email;
+
+    @FindBy (xpath = "//label[@for='agree']")
+    private WebElement agreeButton;
+
+    @FindBy (xpath = "//label[@for='dontagree']")
+    private WebElement dontAgreeButton;
+
+    @FindBy (xpath= "//*[@href='#previous']")
+    private WebElement previousButton;
+
+    @FindBy (xpath = "//*[@href='#next']")
+    private WebElement nextButton;
+
+    @FindBy (xpath="//*[@id=\"steps-uid-0-p-5\"]/div[3]/pre/a[1]")
+    private WebElement yourConsent;
+
+    @FindBy(xpath="//*[@id=\"steps-uid-0-p-5\"]/div[3]/pre/a[1]")
+    private WebElement withdrawalOfConsent;
+
+    @FindBy (xpath="//*[@id=\"steps-uid-0-p-5\"]/div[3]/pre/a[3]")
+    private WebElement obtainingPaperCopies;
+
+    @FindBy (xpath="//*[@id=\"steps-uid-0-p-5\"]/div[3]/pre/a[4]")
+    private WebElement systemRequirements;
+
+    @FindBy (xpath="//*[@id=\"steps-uid-0-p-5\"]/div[3]/pre/a[5]")
+    private WebElement howWeCanReachYou;
+
+
+
+
+
+
+
 }
+
+
+
+
+
--- src/test/resources/e-consent.feature
+++ src/test/resources/e-consent.feature
@@ -1,3 +1,15 @@
 Feature: e-Consent feature
   As a mortgage loan applicant, I want to consent to do business electronically so that I can receive and sign the necessary disclosures related to my loan application. I need to be able to enter my first and last name and email address,
   and I want to be able to easily indicate whether or not I agree to the terms and conditions.
+
+
+
+
+  Scenario: User/Applicant can see First Name , Last Name and Email input boxes on Enconsent Page
+    Given User/Applicant is on Enconsent page
+    When User/ Applicant is entering valid first name ,last name and email adress
+    Then the user can select Yes or No checkbox
+    And click next to move to the next page
+
+
+    Scenario: User / Applicant input inval
